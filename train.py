bb4a305832573a9144e497ccf93153ae3d6dd855
Max Dietsch <diedietschies@gmail.com>
2024-03-04 15:51:27 +0100

    overfit

--- train.py
+++ train.py
@@ -19,7 +19,7 @@ def train(epoch):
     for batch_index, (image,label,_) in enumerate(trainDataLoader):        
         image = image.to(device)
         label = label.to(device) 
-        label = label/9
+        label = label / 9
         Opmimizer.zero_grad()
         score1, cly_map = model(image)
         score2 = cly_map.mean(axis = (1,2,3))
@@ -44,6 +44,7 @@ def evaluation():
     for (image, label, _) in testDataLoader:
         image = image.to(device)
         label = label.to(device)
+        label = label / 9
         with torch.no_grad():
             score, _= model(image)
             all_scores += score.tolist()
