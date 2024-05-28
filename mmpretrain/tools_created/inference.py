56df955bc141f67a7f046307f7688dc944c332f2
diedietschies <diedietschies@gmail.com>
2024-05-28 13:59:03 +0200

    test

--- mmpretrain/tools_created/inference.py
+++ mmpretrain/tools_created/inference.py
@@ -47,7 +47,8 @@ with open("../../../dataset_default/meta/test.txt", "r") as file:
         cly_map = tup[0]
         
         new_path = os.path.join(output_dir, filename, str(label), file_extension)
-        image = transforms.ToPILImage(cly_map)
+        transform_to_image = transforms.ToPILImage()
+        image = transform_to_image(cly_map)
         image.save(new_path)
 
 
