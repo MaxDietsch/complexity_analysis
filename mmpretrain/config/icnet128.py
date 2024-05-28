e174f8bd678ceb3891f9e3ec2f2771fc7a22ffd4
diedietschies <diedietschies@gmail.com>
2024-05-28 09:37:39 +0200

    test

--- mmpretrain/config/icnet128.py
+++ mmpretrain/config/icnet128.py
@@ -1,8 +1,8 @@
 _base_ = [
-        '../../model/icnet128.py',
-        '../../data/default.py',
-        '../../schedule/sgd0_005.py',
-        '../../runtime/default.py'
+        '../model/icnet128.py',
+        '../data/default.py',
+        '../schedule/sgd0_005.py',
+        '../runtime/default.py'
         ]
 
 load_from = None
