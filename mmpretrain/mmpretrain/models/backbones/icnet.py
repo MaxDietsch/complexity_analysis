5aae8278a6aadb362d8c6a3e2c874d2257db9416
diedietschies <diedietschies@gmail.com>
2024-05-28 10:53:32 +0200

    test

--- mmpretrain/mmpretrain/models/backbones/icnet.py
+++ mmpretrain/mmpretrain/models/backbones/icnet.py
@@ -221,7 +221,7 @@ class ICNetBackboneRes18Out128(BaseBackbone):
         #print(score.shape)
 
         score = torch.cat((detail_score, map_score), dim = 1)
-        print(score.shape)
+        #print(score.shape)
 
         return cly_map, score
 
