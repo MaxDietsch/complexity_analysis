05d050b5ef5ea921734dbf0ce4b9b077f0aab6aa
diedietschies <diedietschies@gmail.com>
2024-05-28 13:33:25 +0200

    test

---
+++ mmpretrain/tools_created/inference.py
@@ -0,0 +1,31 @@
+from mmpretrain import ImageClassificationInferencer
+import torch
+from mmengine.config import Config
+import numpy as np
+
+# for classification of healthy or unhealthy
+model = 'icnet128'
+epoch = '12'
+
+
+model_config = f'../config/work_dirs/{model}.py'
+model_pretrained = f'../tools/work_dirs/{model}/epoch_{epoch}.pth'
+
+
+cfg = Config.fromfile(model_config)
+model = ImageClassificationInferencer(model = model_config, pretrained = model_pretrained)
+
+paths, labels = [], []
+
+with open("../../../dataset_default/meta/test.txt", "r") as file:
+    for line in file:
+        path, label = line.strip().split(" ", 1)
+        label = int(label)
+        tup = model.backbone(path)
+        cly_map = tup[0]
+        print(path)
+
+
+            
+
+
