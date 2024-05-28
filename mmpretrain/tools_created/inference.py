6ae1a4b9ee6fb1eceda40f34f1feb7178e72c17b
diedietschies <diedietschies@gmail.com>
2024-05-28 13:36:47 +0200

    test

--- mmpretrain/tools_created/inference.py
+++ mmpretrain/tools_created/inference.py
@@ -14,14 +14,13 @@ model_pretrained = f'../tools/work_dirs/{model}/epoch_{epoch}.pth'
 
 cfg = Config.fromfile(model_config)
 model = ImageClassificationInferencer(model = model_config, pretrained = model_pretrained)
-print(model.model)
 paths, labels = [], []
 
 with open("../../../dataset_default/meta/test.txt", "r") as file:
     for line in file:
         path, label = line.strip().split(" ", 1)
         label = int(label)
-        tup = model.backbone(path)
+        tup = model.model.backbone(path)
         cly_map = tup[0]
         print(path)
 
