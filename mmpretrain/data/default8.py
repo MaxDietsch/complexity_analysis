ad300bbe0929cf684460a217acae212b280b49bf
diedietschies <diedietschies@gmail.com>
2024-05-29 16:49:53 +0200

    regression

--- mmpretrain/data/default8.py
+++ mmpretrain/data/default8.py
@@ -10,8 +10,8 @@ data_preprocessor = dict(
 train_pipeline = [
             dict(type='LoadImageFromFile'),     # read image
             dict(type='Resize', scale=(1024, 1024), interpolation='bicubic'),
-            dict(type = 'RandomFlip', prob = 0.5, direction=['horizontal', 'vertical']),
-            #dict(type = 'ColorJitter', brightness = [0.75, 1.5], contrast = [0.75, 1.5], saturation = [0.75, 1.5], hue = 0.25, backend = 'pillow'),
+            #dict(type = 'RandomFlip', prob = 0.5, direction=['horizontal', 'vertical']),
+            dict(type = 'ColorJitter', brightness = [0.75, 1.5], contrast = [0.75, 1.5], saturation = [0.75, 1.5], hue = 0.25, backend = 'pillow'),
             dict(type='PackInputs'),         # prepare images and labels
         ]
 
