852912a38cf88548a60b26cb4a30b9c20a8bfdef
diedietschies <diedietschies@gmail.com>
2024-05-29 12:23:37 +0200

    regression

--- mmpretrain/schedule/sgd0_005.py
+++ mmpretrain/schedule/sgd0_005.py
@@ -1,5 +1,5 @@
 optim_wrapper = dict(
-    optimizer=dict(type='SGD', lr=0.01, momentum=0.9, weight_decay=0.0001))
+    optimizer=dict(type='SGD', lr=0.1, momentum=0.9, weight_decay=0.0001))
 
 param_scheduler = dict(
     type='MultiStepLR', by_epoch=True, milestones=[50, 100, 150, 200], gamma=0.5)
