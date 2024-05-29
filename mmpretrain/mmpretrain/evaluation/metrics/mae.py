33a5cf8729dae3c38a15911fb7cc35e24e13a251
diedietschies <diedietschies@gmail.com>
2024-05-29 10:07:12 +0200

    regression

--- mmpretrain/mmpretrain/evaluation/metrics/mae.py
+++ mmpretrain/mmpretrain/evaluation/metrics/mae.py
@@ -3,17 +3,15 @@ from mmengine.evaluator import BaseMetric
 from mmpretrain.registry import METRICS
 
 @METRICS.register_module()
-class MAE(BaseMetric):
+class ICNetMAE(BaseMetric):
 
     def __init__(self):
-        super(MAE, self).__init__()
+        super(ICNetMAE, self).__init__()
 
     def process(self, data_batch: Sequence[Dict], data_samples: Sequence[Dict]):
-        print(data_batch)
-        print(data_sample)
         result['pred_score'] = data_sample['pred_score'].cpu()
         result['gt_label'] = data_sample['gt_label'].cpu()
         self.results.append(result)
 
     def compute_metrics(self, results: List):
-        pass
+        print(results)
