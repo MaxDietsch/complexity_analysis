b8db424f9ff042863c737216201efa6145bc5c5d
diedietschies <diedietschies@gmail.com>
2024-05-29 10:49:41 +0200

    regression

--- mmpretrain/tools_created/inference.py
+++ mmpretrain/tools_created/inference.py
@@ -44,7 +44,7 @@ with open("../../../dataset_default/meta/test.txt", "r") as file:
         print(tensor.shape)
 
         label = int(label)
-        tup = model.model.backbone(tensor)
+        tup = model(tensor)
         cly_map = tup[0].squeeze()
         print(cly_map.shape) 
         new_path = os.path.join(output_dir, filename + '-' + str(label) + file_extension)
