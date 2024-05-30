ebcc9a25e0d9e32cbc58f8787f02968516afb3e0
diedietschies <diedietschies@gmail.com>
2024-05-30 12:33:48 +0200

    extend resnet

--- mmpretrain/data/default8.py
+++ mmpretrain/data/default8.py
@@ -10,7 +10,7 @@ data_preprocessor = dict(
 train_pipeline = [
             dict(type='LoadImageFromFile'),     # read image
             dict(type = 'RandomFlip', prob = 0.5, direction=['horizontal', 'vertical']),
-            #dict(type = 'ColorJitter', brightness = [0.75, 1.5], contrast = [0.75, 1.5], saturation = [0.75, 1.5], hue = 0.25, backend = 'pillow'),
+            dict(type = 'ColorJitter', brightness = [0.75, 1.5], contrast = [0.75, 1.5], saturation = [0.75, 1.5], hue = 0.25, backend = 'pillow'),
             dict(type='Resize', scale=(1024, 1024), interpolation='bicubic'),
             dict(type='PackInputs'),         # prepare images and labels
         ]
