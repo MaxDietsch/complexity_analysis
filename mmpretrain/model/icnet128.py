efb96a2d4e7f58834d36ad9570893bb8f479925f
Max Dietsch <diedietschies@gmail.com>
2024-06-04 07:36:32 +0200

    test weighting

--- mmpretrain/model/icnet128.py
+++ mmpretrain/model/icnet128.py
@@ -4,7 +4,7 @@ model = dict(
     neck=None,
     head=dict(
         type='ICNetHead128',
-        loss=dict(type='ICNetLoss', map_weighting = 0.3),
+        loss=dict(type='ICNetLoss', map_weighting = 0.5),
         num_scores = 10,
     ))
 
