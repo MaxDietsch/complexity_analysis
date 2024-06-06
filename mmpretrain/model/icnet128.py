007bf66e18187440fdacfb5ad4b73ea76e843b59
diedietschies <diedietschies@gmail.com>
2024-06-06 16:28:11 +0200

    pretrained

--- mmpretrain/model/icnet128.py
+++ mmpretrain/model/icnet128.py
@@ -1,6 +1,6 @@
 model = dict(
     type='ICNetRegressor',
-    backbone=dict(type='ICNetBackboneRes18Out128', image_size = 1024, size_slam = 128, norm_eval = False),
+    backbone=dict(type='ICNetBackboneRes18Out128', image_size = 1024, size_slam = 128, norm_eval = False, init_cfg = dict(type = 'Pretrained', checkpoint='../work-dir/pretrain_w0.5/epoch_250.pth', prefix = 'backbone'))),
     neck=None,
     head=dict(
         type='ICNetHead128',
